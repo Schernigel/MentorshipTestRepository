e9b98489077eade64cfb27ed55ddbd551a76d038
UserSer <UserSer@DESKTOP-9Q8NKBR>
2018-02-11 14:15:50 +0200

    Added CustomHtmlHelpers, enjine.js, css file

---
+++ Mentorship.Web.CustomButton/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Mentorship.Web.CustomButton.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
