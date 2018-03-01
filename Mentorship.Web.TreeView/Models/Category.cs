c7ac75d0055ac6bfc15c5e35f20f6eec2374ae5b
Sergii_Chernigel <Sergii_Chernigel@EPUALVIW0650>
2018-03-01 17:41:13 +0200

    Added ITreeViewModel

--- Mentorship.Web.TreeView/Models/Category.cs
+++ Mentorship.Web.TreeView/Models/Category.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using Mentorship.Helper.TreeView;
 
 namespace Mentorship.Web.TreeView.Models
 {
-    public class Category
+    public class Category : ITreeViewModel
     {
         public int ID { get; set; }
 
@@ -15,6 +16,8 @@ namespace Mentorship.Web.TreeView.Models
         public string Description { get; set; }
 
         public int? ParentId { get; set; }
+
+        public string Disabled { get; set; }
 
         [ForeignKey("ParentId")]
         public virtual Category Parent { get; set; }
---
+++ Mentorship.Web.TreeView/TreeViewContext.cs
@@ -0,0 +1,16 @@
+﻿using Mentorship.Web.TreeView.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Mentorship.Web.TreeView
+{
+    public class TreeViewContext : DbContext
+    {
+        public TreeViewContext() { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
